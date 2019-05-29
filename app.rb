1d5c2c8d585d7c3c2ec7cf3a414c58514251bcc9
denis281542 <deniskuimov3@gmail.com>
2019-05-29 15:23:41 +0300

    Init datebase. db

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ require 'sqlite3'
 
 
 def init_db
-	@db = SQlite3::Database.new 'leprosorium.db'
+	@db = SQLite3::Database.new 'leprosorium.db'
 	@db.results_as_hash = true
 end
 
-#before do
-#	init_db
-#end
+before do
+	init_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
