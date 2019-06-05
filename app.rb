7cc67fbd9dd93eea3d0aaa74476bf6255ea61f19
denis281542 <deniskuimov3@gmail.com>
2019-06-05 18:24:13 +0300

    Add index.erb

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
 
 get '/new' do
@@ -40,6 +40,7 @@ post '/new' do
   	return erb :new
   end
 
+  #сохранение данных в БД
   @db.execute 'insert into Posts (content, created_date) values (?, datetime ())', [content]
 
   erb "You typed: #{content}"
