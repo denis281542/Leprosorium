6edde1223abb82ad152768079361551094b199fd
denis281542 <deniskuimov3@gmail.com>
2019-06-05 18:42:02 +0300

    redirect to

--- app.rb
+++ app.rb
@@ -46,5 +46,8 @@ post '/new' do
   #сохранение данных в БД
   @db.execute 'insert into Posts (content, created_date) values (?, datetime ())', [content]
 
+  #перенаправление на главную страницу
+
+  redirect to '/'
   erb "You typed: #{content}"
 end
