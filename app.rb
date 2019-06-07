31c11960aa034f34399e94fe79ccc151346bc487
denis281542 <deniskuimov3@gmail.com>
2019-06-07 21:41:15 +0300

    Add simple /details/... post_id

--- app.rb
+++ app.rb
@@ -54,10 +54,26 @@ post '/new' do
   #вывод информации о посте
 
   get '/details/:post_id' do
+ 
+  		#получаем переменную из urla
   		post_id = params[:post_id]
 
+  		#получем список постов (будет только один пост)
 		results = @db.execute 'select * from Posts where id = ?', [post_id]
+		
+		#выбираем этот один пост в переменную @row
 		@row = results[0]
 
+		#возвращаем представление details.erb
   		erb :details
   end 
+
+
+  #обработчик post запроса /details/...(браузер отправляет данные на сервер, мы их принимаем)
+  post '/details/:post_id' do
+#получаем переменную из urla
+	post_id = params[:post_id]
+  	content = params[:content]
+
+  	erb "You typed comment #{content} for post#{post_id}"
+  	end
